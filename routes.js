009bd0d7c752e91e8eac91e1db881bcae1e0fe40
vagrant <vagrant@precise32.(none)>
2014-08-07 13:51:40 +0000

    check

--- routes.js
+++ routes.js
@@ -1,8 +1,6 @@
 module.exports = function(app) {
 	var handlers = require('./handlers');
 	app.get('/', handlers.UvaHome);
-	app.get('/watch', handlers.watch);
-	app.get('/bootstrap', handlers.bootstrap);
 	app.get('/pinterless', handlers.pinterless);
 
 	// Posts
